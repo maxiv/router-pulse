1203827c349b34e9a7cbae836bb7a84be2feaf3d
Maxim Ivanchenko <maxim@ivanchenko.pw>
2016-01-01 18:11:12 +0300

    SMS notification charset fix

--- app/Http/Models/SMSNotifier.php
+++ app/Http/Models/SMSNotifier.php
@@ -5,7 +5,7 @@ namespace App\Http\Models;
 class SMSNotifier
 {
     public function send($login, $password, $phones, $message) {
-        $result = file_get_contents('https://smsc.ru/sys/send.php?login=' . urlencode($login) . '&psw=' . urlencode($password) . '&phones=' . urlencode($phones) . '&mes=' . urlencode($message));
+        $result = file_get_contents('https://smsc.ru/sys/send.php?login=' . urlencode($login) . '&psw=' . urlencode($password) . '&phones=' . urlencode($phones) . '&mes=' . urlencode($message) . '&charset=utf-8');
         return $result;
     }
 }
